a58f763435dca3e91975d55abc1b2682e7583284
Yichen Huang <yichen41@illinois.edu>
2023-10-24 22:55:01 -0500

    Created MVC controller for register user and login user

--- backend/server.js
+++ backend/server.js
@@ -8,4 +8,7 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: "Welcome to the support desk api" })
 })
 
+// Routes
+app.use("/api/users", require('./routes/userRoutes'))
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
