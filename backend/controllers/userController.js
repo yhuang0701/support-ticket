2c5b829bfa23ddf190c0c92691967ad63cd3de8c
Yichen Huang <yichen41@illinois.edu>
2023-10-24 23:09:42 -0500

    Added body parser and simple error validation for user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,20 @@
+// @desc    Register a new user
+// @route   /api/users/
+// @access  Public
 const registerUser = (req, res) => {
+  const { name, email, password } = req.body
+
+  // Validation
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "Please include all fields" })
+  }
+
   res.send('Register Route')
 }
 
+// @desc    Login a new user
+// @route   /api/users/login
+// @access  Public
 const loginUser = (req, res) => {
   res.send('Login Route')
 }
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
+app.use(express.json()) // allow us to send raw json
+app.use(express.urlencoded({ extended: false })) // accept the url-encoded form
+
 app.get('/', (req, res) => {
   res.status(200).json({ message: "Welcome to the support desk api" })
 })
