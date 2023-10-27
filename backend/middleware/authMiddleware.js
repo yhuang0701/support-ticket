855358a3df4ac935d9ba9c19fd4249d90a823e0f
Yichen Huang <yichen41@illinois.edu>
2023-10-26 22:39:55 -0500

    Create auth middleware to protect the personal route at /me

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(' ')[1]
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET) // decoded will contain the user id
+      // Get user from token
+      req.user = await User.findById(decoded.id).select('-password') // exclude the password
+
+      next()
+    } catch (error) {
+      console.log(error)
+
+    }
+  }
+
+  if (!token) {
+    res.status(401)
+    throw new Error('Not authorized')
+  }
+})
+
+module.exports = { protect }
